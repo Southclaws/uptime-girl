a0c4ef6a168963df453a2c1b98d795c61835b5c8
Barnaby Keene <hello@southcla.ws>
2018-12-22 16:10:24 +0000

    implemented uptimerobot api calls into watcher code

--- service/service.go
+++ service/service.go
@@ -1,10 +1,14 @@
 package service
 
 import (
+	"context"
 	"strings"
 
 	"github.com/Southclaws/dockwatch"
 	"github.com/docker/docker/client"
+	"go.uber.org/zap"
+
+	"github.com/Southclaws/uptime-girl/uptimerobot"
 )
 
 // Labels that the service looks out for
@@ -21,6 +25,7 @@ type Config struct {
 // App stores app state
 type App struct {
 	docker *client.Client
+	uptime *uptimerobot.Client
 }
 
 // Initialise prepares the service
@@ -30,31 +35,36 @@ func Initialise(config Config) (app *App, err error) {
 		return
 	}
 
-	app = &App{docker: docker}
+	app = &App{
+		docker: docker,
+		uptime: uptimerobot.New(config.UptimeRobotKey),
+	}
 
 	return
 }
 
 // Start runs the service and blocks until failure
-func (app *App) Start() (err error) {
+func (app *App) Start(ctx context.Context) (err error) {
 	w := dockwatch.New(app.docker)
 
-	f := func() (e error) {
+	f := func() error {
+		var e error
 		select {
-		case evt := <-w.Events:
-			endpoint := app.react(evt)
-			if evt.Type == dockwatch.EventTypeCreate {
-				// create monitor
-			} else if evt.Type == dockwatch.EventTypeDelete {
-				// delete monitor
-			}
+		case <-ctx.Done():
+			return ctx.Err()
 		case e = <-w.Errors:
+
+		case evt := <-w.Events:
+			e = app.react(evt)
 		}
-		return
+		if e != nil {
+			zap.L().Error("error occurred", zap.Error(e))
+		}
+		return nil
 	}
 
 	for {
-		err = f()
+		f()
 		if err != nil {
 			break
 		}
@@ -63,7 +73,37 @@ func (app *App) Start() (err error) {
 	return
 }
 
-func (app *App) react(e dockwatch.Event) (endpoint string) {
+func (app *App) react(e dockwatch.Event) (err error) {
+	endpoint := app.getEndpoint(e)
+	if endpoint == "" {
+		return
+	}
+
+	if e.Type == dockwatch.EventTypeCreate {
+		app.uptime.NewMonitor(uptimerobot.Monitor{
+			URL:          endpoint,
+			FriendlyName: endpoint,
+		})
+	}
+
+	if e.Type == dockwatch.EventTypeDelete {
+		var monitors []uptimerobot.Monitor
+		monitors, err = app.uptime.GetMonitors()
+		if err != nil {
+			return
+		}
+		for _, m := range monitors {
+			if m.URL == endpoint {
+				app.uptime.DeleteMonitor(m.ID)
+				break
+			}
+		}
+	}
+
+	return
+}
+
+func (app *App) getEndpoint(e dockwatch.Event) (endpoint string) {
 	if endpoint, ok := e.Container.Labels[LabelMonitorEndpoint]; ok {
 		return endpoint
 	}
